13417decbd9c3a155139081ce70589aa265325fd
Tamara Koch <T.Koch@student.uibk.ac.at>
2026-02-01 20:23:17 +0100

    Implementing tabs with hover and active styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, inject, ViewChild, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
+import { PageLayout } from "../../projects/page-layout/src/public-api";
+import { routes } from './app.routes';
+import { CommonModule } from '@angular/common';
+
+export class Tab {
+  id: string;
+  label: string;
+
+  constructor(id: string) {
+    this.id = id;
+    this.label = this.pathLabelMapping.get(id) ?? '--';
+  }
+
+  private pathLabelMapping: Map<string, string> = new Map([
+    ['introduction', 'Vorstellung'],
+    ['experience', 'Erfahrung'],
+    ['education', 'Ausbildung'],
+    ['skills', 'Fähigkeiten'],
+    ['documents', 'Dokumente'],
+    ['about-me', 'Über mich'],
+    ['contact', 'Kontaktdaten']
+  ]);
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PageLayout, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
+
 export class AppComponent {
   title = 'application-frontend';
+  tabs = routes
+  .filter(route => typeof route.path === 'string' &&
+    route.path !== '**' &&
+    route.path !== '')
+  .map(route => new Tab(route.path!))
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'introduction',
+        component: IntroductionComponent,
+    },
+    {
+        path: 'experience',
+        component: ExperienceComponent
+    },
+    {
+        path: 'education',
+        component: EducationComponent
+    },
+    {
+        path: 'skills',
+        component: SkillsComponent
+    },
+    {
+        path: 'documents',
+        component: DocumentsComponent
+    },
+    {
+        path: 'about-me',
+        component: AboutMeComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'introduction',
+    },
+];
