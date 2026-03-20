ea2841b65495c1b4d6814f544ef080384655ac01
Tamara Koch <T.Koch@student.uibk.ac.at>
2026-03-20 13:46:35 +0100

    add-documents-content: provide downloads of all downloads that are configured in the experience and education files. The cover letter and the curriculum vitae need to be created when all information e.g. 'about-me' are finished implemented. Added a 'ToDo' reminder.

--- src/app/pipes/get-formatted-content.pipe.ts
+++ src/app/pipes/get-formatted-content.pipe.ts
@@ -1,6 +1,7 @@
 import { inject, Pipe, PipeTransform } from '@angular/core';
-import { IntroductionContent, Paragraph } from '../data/introduction';
+import { Paragraph } from '../data/introduction';
 import { LocalizationService } from '../services/localization.service';
+import { Localized } from '../../../projects/time-line/src/public-api';
 
 @Pipe({
   name: 'getFormattedContent',
@@ -8,9 +9,9 @@ import { LocalizationService } from '../services/localization.service';
 })
 export class GetFormattedContentPipe implements PipeTransform {
   private i18n = inject(LocalizationService);
-  
-  transform(value: IntroductionContent): string {
-    const content: Paragraph[] = this.i18n.get(value);
+
+  transform(value: Localized<Paragraph[]>): string {
+    const content: Paragraph[] = this.i18n.get<Paragraph[]>(value);
     const contentWithProperSentenceEnding = content.map(p => p.map(s => s.endsWith('.') ? s : s + '.').join(' '));
     return contentWithProperSentenceEnding.map(p => `<p>${p}</p>`).join(' ');
   }
